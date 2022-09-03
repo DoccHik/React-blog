46195b155986cbd0ed8df243f97e1b4ee4d7dd95
DoccHik <evgeniy161102@gmail.com>
2022-09-03 15:23:12 +0300

    Made form controlable

--- src/Components/BlogContent/components/AddPostForm/AddPostForm.jsx
+++ src/Components/BlogContent/components/AddPostForm/AddPostForm.jsx
@@ -3,41 +3,66 @@ import React from "react";
 import "../AddPostForm/AddPostForm.css";
 
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
-const AddPostForm = ({ hideAddPostForm }) => {
-  return (
-    <>
-      <div className="overlay">
-        <button onClick={hideAddPostForm} className="overlay-close__btn">
-          <CloseRoundedIcon />
-        </button>
-        <form action="" className="add-post-form">
-          <h2 className="add-post__heading">Новый пост</h2>
-          <div>
-            <input
-              className="add-post-title"
-              type="text"
-              name="postTitle"
-              placeholder="Напишите название"
-            />
-          </div>
-          <div>
-            <textarea
-              className="add-post-description"
-              name="postDescription"
-              placeholder="Напишите текст"
-            />
-          </div>
-          <div>
-            <button className="add-post-btn" type="button">
-              Опубликовать пост
-            </button>
-          </div>
-        </form>
-      </div>
-    </>
-  );
-};
+class AddPostForm extends React.Component {
+  state = {
+    postTitle: "",
+    postDescription: "",
+  };
+
+  handlePostTitleChange = (e) => {
+    // перехватываем значение в inputTitle через event.target.value
+    this.setState({
+      // Добавляем значения в inputTitle.value
+      postTitle: e.target.value,
+    });
+  };
+
+  handlePostDescriptionChange = (e) => {
+    // перехватываем значение в inputDescription через event.target.value
+    this.setState({
+      // Добавляем значения в inputDescription.value
+      postDescription: e.target.value,
+    });
+  };
+  render() {
+    const hideAddPostForm = this.props.hideAddPostForm;
+    return (
+      <>
+        <div className="overlay">
+          <button onClick={hideAddPostForm} className="overlay-close__btn">
+            <CloseRoundedIcon />
+          </button>
+          <form action="" className="add-post-form">
+            <h2 className="add-post__heading">Новый пост</h2>
+            <div>
+              <input
+                className="add-post-title"
+                type="text"
+                name="postTitle"
+                placeholder="Напишите название"
+                value={this.state.postTitle}
+                onChange={this.handlePostTitleChange}
+              />
+            </div>
+            <div>
+              <textarea
+                className="add-post-description"
+                name="postDescription"
+                placeholder="Напишите текст"
+                value={this.state.postDescription}
+                onChange={this.handlePostDescriptionChange}
+              />
+            </div>
+            <div>
+              <button className="add-post-btn" type="button">
+                Опубликовать пост
+              </button>
+            </div>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
 
 export default AddPostForm;
-
-// ! https://youtu.be/czPUlOI5kwU
